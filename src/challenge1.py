d8b6e3d90d23498b74bf1b54c5c2a993f176e8d6
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2018-06-06 15:41:10 -0600

    clarify challenge1 instructions

--- src/challenge1.py
+++ src/challenge1.py
@@ -10,38 +10,40 @@ To test this script in the terminal, go to root directory
 (programming_challenges/) and run `make test1`.
 '''
 
-def sort_elements(iter):
+def sort_elements(iterable):
     '''
-    Sorts all the elements in a list in ascending order
+    Sorts all the elements in a list in ascending order (without using other
+    built-in sort functions or methods)
 
     Input
-        iter: LIST - a list of numeric values
+        iterable: LIST - a list of numeric values
     Output
         a sorted list of numeric values
     '''
     pass
 
 
-def sort_unique(iter):
+def sort_unique(iterable):
     '''
-    Sort all elements in a list and remove all redundant values
+    Sort all elements in a list and remove all redundant values (without using
+    other built-in sort functions or methods)
 
     Input
-        iter: LIST - a list of numeric values
+        iterable: LIST - a list of numeric values
     Output
         a sorted list of numeric values
     '''
     pass
 
 
-def sort_mixed_type(iter):
+def sort_mixed_type(iterable):
     '''
     Sort a list that contains both numeric and string data types. If two
     element hold the same numeric value but are of different data types, the
     numeric data type should precede the string type.
 
     Input
-        iter: LIST - a list of string and numeric data types
+        iterable: LIST - a list of string and numeric data types
             e.g. iter = ['4.15', 3, 507, '12', 4.15, 13.3]
     Output
         a sorted list; e.g. iter = [3, 4.15, '4.15', '12', 13.3, 507]
@@ -61,12 +63,13 @@ def is_palindrome(word):
     pass
 
 
-def reverse(iter):
+def reverse(iterable):
     '''
-    Reverses the order of either a list or a string
+    Reverses the order of either a list or a string (without using other
+    built-in reverse functions or methods)
 
     Input
-        iter: LIST or STR
+        iterable: LIST or STR
     Output
         a reversed list or string
     '''
